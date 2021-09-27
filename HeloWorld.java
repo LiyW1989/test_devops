b5f0f9a12947cf412220f3d47673e3913cb2832a
liyW1989 <wanglong-1989@163.com>
2021-09-27 22:13:41 +0800

    helloworld commit

---
+++ HeloWorld.java
@@ -0,0 +1,13 @@
+/**
+ * @Project: test_devops
+ * @Site: http://www.lemonban.com
+ * @Author: l
+ * @Create: 2021-09-27 22:05
+ * @Desc：
+ **/
+
+public class HeloWorld {
+    public static void main(String[] args) {
+        System.out.println("Lemmon Hello");
+    }
+}
